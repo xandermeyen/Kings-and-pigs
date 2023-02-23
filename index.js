1b8658c55ca40f9cc53d5302f5313ed88a2b1c70
xandermeyen <xander.meyen@gmail.com>
2023-02-23 21:31:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,117 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+const parsedCollisions = collisionsLevel1.parse2D()
+const collisionBlocks = parsedCollisions.createObejectsFrom2D()
+
+const backgroundLevel1 = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    imageSrc: './img/backgroundLevel1.png'
+})
+
+const player = new Player({
+    collisionBlocks,
+    imageSrc: './img/king/idle.png',
+    frameRate: 11,
+    animations: {
+        idleRight: {
+            frameRate: 11,
+            frameBuffer: 4,
+            loop: true,
+            imageSrc: './img/king/idle.png',
+        },
+        idleLeft: {
+            frameRate: 11,
+            frameBuffer: 4,
+            loop: true,
+            imageSrc: './img/king/idleLeft.png',
+        },
+        runRight: {
+            frameRate: 8,
+            frameBuffer: 8,
+            loop: true,
+            imageSrc: './img/king/runRight.png',
+        },
+        runLeft: {
+            frameRate: 8,
+            frameBuffer: 8,
+            loop: true,
+            imageSrc: './img/king/runLeft.png',
+        },
+        enterDoor: {
+            frameRate: 8,
+            frameBuffer: 8,
+            loop: false,
+            imageSrc: './img/king/enterDoor.png',
+            onComplete: () => {
+                console.log('completed animation')
+                gsap.to(overlay, {
+                    opacity: 1
+                })
+            },
+        },
+    },
+})
+
+const doors = [
+    new Sprite({
+        position: {
+            x: 767,
+            y: 270,
+        },
+        imageSrc: './img/doorOpen.png',
+        frameRate: 5,
+        frameBuffer: 9,
+        loop: false,
+        autoplay: false,
+    })
+]
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
+
+const overlay = {
+    opacity: 0,
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+
+    backgroundLevel1.draw()
+    collisionBlocks.forEach((collisionBlock) => {
+        collisionBlock.draw()
+    })
+
+    doors.forEach((door) => {
+        door.draw()
+    })
+
+    player.handleInput(keys)
+    player.draw()
+    player.update()
+
+    c.save()
+    c.globalAlpha = overlay.opacity
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    c.restore()
+}
+
+animate()
+
+
